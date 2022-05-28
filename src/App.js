2cb4b80b38511c6f18b48f2db1ef40d30fe37273
Bhushan <bhushan61399@gmail.com>
2022-05-29 00:34:57 +0530

    statefull component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,18 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return (
-    <> {/* fragment */}
-      <Part part={props.course.parts[0]}/>
-      <Part part={props.course.parts[1]}/>
-      <Part part={props.course.parts[2]}/>
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  let sum = 0;
-  props.course.parts.forEach(parts => {
-    sum += parts.exercises;
-  });
-  return (
-    <p>Number of exercises {sum}</p>
-  )
-}
-
-const App = (prop) => {
-  const {counter} = prop;
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
   
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-      <div>{counter}</div>
+      {counter}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-let counter = 0;
-
-const refresh = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App counter={counter} />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
